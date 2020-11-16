cd7574d90e0165e9b550a2989a607e256a6171a4
KOSEKI Yoshinori <koseki.y@gmail.com>
2020-11-16 16:22:00 +0900

    new

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# benchmark-OpenVINO
+# benchmark OpenVINO
+
+- Raspberry Pi 3B : ARM Cortex-A53, 4コア, 1.2GHz
+- Mac mini 2018 : Core i5-8500B, 6コア, 3.00GHz
+- MacBook Pro 2018 : Core i5-8259, 8コア, 2.30GHz
+- NUC10i5 : Core i5-10210U, 8コア, 1.60GHz
+
+![table](images/table.png)
+![2](images/2.png)
+![3](images/3.png)
+
+## tools
+
+- [Bencjmark C++ Tool](https://docs.openvinotoolkit.org/latest/openvino_inference_engine_samples_benchmark_app_README.html)
---
+++ benchmark.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: UTF-8
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+csvfile = "benchmark.cvs"
+
+df = pd.read_csv(csvfile)
+length = len(df)
+
+def make_chart(idx):
+
+    df2 = df.loc[idx]
+    testname = df2[df2.index[0]]
+    index = df2.index[1:]
+    data = [df2[i] for i in index]
+    colorlist =  ["tab:blue" if i % 2 == 0 else "tab:orange" for i in range(len(data))]
+
+    fig = plt.figure(figsize=(10, 2.5), dpi=100)
+
+    plt.barh(index, data, color=colorlist)
+    plt.grid(axis='x')
+    plt.title(testname)
+    plt.subplots_adjust(left=0.35, right=0.98)
+    plt.gca().invert_yaxis() # 上下並びを逆順に
+
+    plt.pause(5)
+    # plt.show()
+    fig.savefig("images/" + str(idx) + ".png")
+
+make_chart(2)
+make_chart(3)
+
+import pdb; pdb.set_trace()
+
+### Local Variables: ###
+### truncate-lines:t ###
+### End: ###
+
+
