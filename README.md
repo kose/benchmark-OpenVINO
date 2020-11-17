23030a83a54319d181e205d2bef9910b3b61caa9
KOSEKI Yoshinori <koseki.y@gmail.com>
2020-11-17 09:24:46 +0900

    improve bar chart

--- README.md
+++ README.md
@@ -6,8 +6,16 @@
 - NUC10i5 : Core i5-10210U, 8コア, 1.60GHz
 
 ![table](images/table.png)
-![2](images/2.png)
 ![3](images/3.png)
+![2](images/2.png)
+
+## Reference
+
+- [human-pose-estimation-0001](https://docs.openvinotoolkit.org/2019_R1/_human_pose_estimation_0001_description_human_pose_estimation_0001.html)
+- [Human Pose Estimation C++ Demo](https://docs.openvinotoolkit.org/latest/omz_demos_human_pose_estimation_demo_README.html)
+- [YOLOv3 を OpenVINO™ ツールキットで使用する IR 形式へ変換してみよう](https://macnicago.zendesk.com/hc/ja/articles/360042709871-YOLOv3-%E3%82%92-OpenVINO-%E3%83%84%E3%83%BC%E3%83%AB%E3%82%AD%E3%83%83%E3%83%88%E3%81%A7%E4%BD%BF%E7%94%A8%E3%81%99%E3%82%8B-IR-%E5%BD%A2%E5%BC%8F%E3%81%B8%E5%A4%89%E6%8F%9B%E3%81%97%E3%81%A6%E3%81%BF%E3%82%88%E3%81%86)
+- [Object Detection YOLO* V3 C++ Demo, Async API Performance Showcase](https://docs.openvinotoolkit.org/latest/omz_demos_object_detection_demo_yolov3_async_README.html)
+
 
 ## tools
 
--- benchmark.py
+++ benchmark.py
@@ -3,36 +3,48 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import re
+import numpy as np
 
 csvfile = "benchmark.cvs"
 
-df = pd.read_csv(csvfile)
-length = len(df)
+df = pd.read_csv(csvfile, index_col=0)
 
 def make_chart(idx):
 
-    df2 = df.loc[idx]
-    testname = df2[df2.index[0]]
-    index = df2.index[1:]
-    data = [df2[i] for i in index]
-    colorlist =  ["tab:blue" if i % 2 == 0 else "tab:orange" for i in range(len(data))]
+    title = df.index[idx]
+    df2 = df.loc[title]
 
-    fig = plt.figure(figsize=(10, 2.5), dpi=100)
+    cols = len(df2)
+    index = [df2.index[i] for i  in range(0, cols, 2)]
+    index = [re.sub(" / [0-9 a-zA-Z]*", "", str) for str in index]
+    yolo = [df2[i] for i  in range(0, cols, 2)]
+    hpe = [df2[i] for i  in range(1, cols, 2)]
 
-    plt.barh(index, data, color=colorlist)
-    plt.grid(axis='x')
-    plt.title(testname)
-    plt.subplots_adjust(left=0.35, right=0.98)
+    fig = plt.figure(figsize=(10, 2.5))
+	
+    bar_width = 0.45
+    x = np.arange(len(index))
+	
+    plt.title(title)
+    plt.barh(x - bar_width/2, yolo, bar_width, color="tab:blue", label="YoLo v3")
+    plt.barh(x + bar_width/2, hpe, bar_width, color="tab:orange", label="Human Pose Estimation")
+    plt.legend()
+    plt.subplots_adjust(left=0.16, right=0.98)
     plt.gca().invert_yaxis() # 上下並びを逆順に
+    plt.yticks(x, index)
+    plt.grid(axis='x')
 
     plt.pause(5)
-    # plt.show()
     fig.savefig("images/" + str(idx) + ".png")
 
+##
+##
+##
 make_chart(2)
 make_chart(3)
 
-import pdb; pdb.set_trace()
+# import pdb; pdb.set_trace()
 
 ### Local Variables: ###
 ### truncate-lines:t ###
